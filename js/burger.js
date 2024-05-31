f904fde91b75ef2c9a77865c0dfc420c53769f9f
Yulia Solnechnaya <yulia.s@mintymint.net>
2024-05-31 17:47:52 +0300

    burger js fixed

--- js/burger.js
+++ js/burger.js
@@ -2,8 +2,18 @@ const burgerMenuIcon = document.querySelector('.burger-menu-icon');
 const burgerMenu = document.querySelector('.burger-menu');
 const body = document.querySelector('body');
 
-burgerMenuIcon.addEventListener('click', function() {
+burgerMenuIcon.addEventListener('click', function(event) {
   burgerMenuIcon.classList.toggle('open-menu');
   burgerMenu.classList.toggle('open-menu');
   body.classList.toggle('fixed-page');
+  event.stopPropagation(); 
+});
+
+document.addEventListener('click', function(event) {
+  if (!burgerMenu.contains(event.target) && !burgerMenuIcon.contains(event.target)) {
+  
+    burgerMenuIcon.classList.remove('open-menu');
+    burgerMenu.classList.remove('open-menu');
+    body.classList.remove('fixed-page');
+  }
 });
